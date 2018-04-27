fa220d8cda47efe5600c870b1970aa8b26530ae6
Son-Guhun <criowoc@hotmail.com>
2018-04-27 20:58:10 -0300

    Added new TriggerObjectInUseException
    Used for errors regarding the deletion of objects which have references in the script.

--- my_exceptions.py
+++ my_exceptions.py
@@ -1,2 +1,23 @@
 class TriggerSyntaxException(Exception):
     pass
+
+
+class TriggerObjectInUseException(TriggerSyntaxException):
+    """
+    Args:
+        msg (str): Human readable string describing the exception.
+        usage_set (set[TriggerEditorObject]): A set of (all) objects which reference the object that raised the error.
+
+    Attributes:
+        msg (str): Human readable string describing the exception.
+        usage_set (set[TriggerEditorObject]): A set of (all) objects which reference the object that raised the error.
+    """
+
+    def __init__(self, *args):
+        super(TriggerObjectInUseException, self).__init__(self, *args)
+        self._usage_set = args[1]
+
+    @property
+    def usage_set(self):
+        return self._usage_set
+    pass
